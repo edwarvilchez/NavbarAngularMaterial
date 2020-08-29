5977cd3f6fca3ad6bd7e5b74f0aefbeb194b71a3
edwarvilchez <edwarvilchez1977@gmail.com>
2020-08-29 16:28:41 -0400

    Navbar con Angular8 y Angular Material

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AddUserComponent } from './add-user/add-user.component';
+import { ViewUserComponent } from './view-user/view-user.component';
+import { DeleteUserComponent } from './delete-user/delete-user.component';
+import { UpdateUserComponent } from './update-user/update-user.component';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'add-user',
+    component: AddUserComponent
+  },
+  {
+    path: 'view-user',
+    component: ViewUserComponent
+  },
+  {
+    path: 'delete-user',
+    component: DeleteUserComponent
+  },
+  {
+    path: 'update-user',
+    component: UpdateUserComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
